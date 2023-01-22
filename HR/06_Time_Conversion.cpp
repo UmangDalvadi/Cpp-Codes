ea7a5322e7808a124614da341edbbb43ebfce050
UmangDalvadi <umangdalvadi8@gmail.com>
2023-01-22 12:14:12 +0530

    add

--- HR/06_Time_Conversion.cpp
+++ HR/06_Time_Conversion.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
+#include <string.h>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    char s;
+    string s;
     cin >> s;
-    for (int i = 0; i < 2; i++)
+    // for (int i = 0; i < 2; i++)
+    // {
+    if (s[8] == 'A')
     {
-        
+        // cout << endl;
+        // cout << "A" << endl;
+        if (s[0] == '1' && s[1] == '2')
+        {
+            s[0] = '0';
+            s[1] = '0';
+        }
     }
-    
+    else if (s[8] == 'P')
+    {
+        // cout << endl;
+        // cout << "P" << endl;
+        if (s[0] == '1' && s[1] == '2')
+        {
+            // continue;
+        }
+        else if (!(s[0] == '1' && s[1] == '2'))
+        {
+            s[0] += 1;
+            s[1] += 2;
+            // s[1]++;
+        }
+    }
+
+    string temp;
+    for (int i = 0; i < 8; i++)
+    {
+        temp[i] = s[i];
+    }
+    for (int i = 0; i < 8; i++)
+    {
+
+        cout << temp[i];
+    }
 }
