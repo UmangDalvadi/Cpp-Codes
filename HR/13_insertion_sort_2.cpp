be594a1aea69489dd00e82bddaca1ff15973e8fc
UmangDalvadi <umangdalvadi8@gmail.com>
2023-01-24 11:43:14 +0530

    add

---
+++ HR/13_insertion_sort_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int n, j;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        j = i - 1;
+        while (arr[j] > key && j >= 0)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
