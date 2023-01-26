21220c61baec327ff3b312b0890f7910d180df3d
UmangDalvadi <umangdalvadi8@gmail.com>
2023-01-26 09:21:52 +0530

    add

---
+++ HR/14_Divisible_Sum_Pairs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int n, k, count = 0;
+    cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if ((arr[i] + arr[j]) % k == 0)
+            {
+                count++;
+            }
+        }
+    }
+    cout << count;
+
+    return 0;
+}
