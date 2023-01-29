46ede5f0c71498e4ff5f6430d3054a47885795b1
UmangDalvadi <umangdalvadi8@gmail.com>
2023-01-30 01:04:14 +0530

    add

--- HR/19_Sales_by_Match.cpp
+++ HR/19_Sales_by_Match.cpp
@@ -4,38 +4,53 @@
 
 using namespace std;
 
+void sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
+
 int main()
 {
-    int n, count = 0;
+    int n, count = 1, pair = 0;
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    for (int i = 0; i < n - 1; i++)
+    sort(arr, n);
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     // cin >> arr[i];
+    //     cout << arr[i] << " ";
+    // }
+
+    for (int i = 0; i < n; i++)
     {
-        // for (int k = 0; k < 100; k++)
-        // {
-        //     if (arr[i] == temp[k])
-        //     {
-        //         i++;
-        //         break;
-        //     }
-        // }
 
-        for (int j = i + 1; j < n; j++)
+        if (arr[i] == arr[i + 1])
         {
-            if (arr[i] == arr[j])
-            {
-                count++;
-                // temp[i] = arr[j];
-            }
+            count++;
         }
-        // i++;
+        else
+        {
+            pair = pair + (count / 2);
+            count = 1;
+        }
     }
-
-    cout << count;
+    cout << pair;
 
     return 0;
 }
