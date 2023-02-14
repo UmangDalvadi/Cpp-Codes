f7a40eb2a5ed3fb70256b110d120b8a76da345bd
UmangDalvadi <umangdalvadi8@gmail.com>
2023-02-14 23:55:05 +0530

    add

---
+++ HR/38_Utopian_Tree.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int n, arr[60], count = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    long int arr2[70];
+    for (int i = 0; i <= 60; i++)
+    {
+        if (i % 2 == 0)
+        {
+            count = count + 1;
+            arr2[i] = count;
+        }
+        else
+        {
+            count = count * 2;
+            arr2[i] = count;
+        }
+    }
+    int i = 0, j = 0;
+    while (i <= 61)
+    {
+        if (arr[j] == i)
+        {
+            cout << arr2[i]<<endl;
+            j++;
+            i = 0;
+        }
+        i++;
+    }
+
+    return 0;
+}
