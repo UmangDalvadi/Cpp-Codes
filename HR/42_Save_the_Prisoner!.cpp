c7854e2eed3844582b0d63adce773a5527eb5668
UmangDalvadi <umangdalvadi8@gmail.com>
2023-03-06 23:46:30 +0530

    add

--- HR/42_Save_the_Prisoner!.cpp
+++ HR/42_Save_the_Prisoner!.cpp
@@ -8,38 +8,45 @@ int main()
 {
     int n;
     cin >> n;
-    int temp[n];
+    // int temp[n];
 
     for (int i = 0; i < n; i++)
     {
-        int p, s, c, count;
-            // 3     7   3
-            // 7    19   2
+        long long p, s, c, count;
+        // 3     7   3
+        // 7    19   2
         cin >> p >> s >> c;
-        count = c;
-        for (int j = 0; j < s; j++)
+        // count = c;
+        long long res = (s + c - 1) % p;
+        if (res == 0)
         {
-            if(j==0)
-            {
-                continue;
-            }
-            if (count > p)
-            {
-                count = 1;
-            }
-            else
-            {
-                count++;
-            }
+            res = p;
         }
+        cout << res << endl;
+
+        // for (int j = 0; j < s; j++)
+        // {
+        //     if(j==0)
+        //     {
+        //         continue;
+        //     }
+        //     if (count > p)
+        //     {
+        //         count = 1;
+        //     }
+        //     else
+        //     {
+        //         count++;
+        //     }
+        // }
         // cout << count;
-        temp[i] = count;
+        // temp[i] = count;
     }
     // cout << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << temp[i] << endl;
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << temp[i] << endl;
+    // }
 
     return 0;
 }
